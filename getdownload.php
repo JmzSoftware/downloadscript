4d3483b84bb001d168901712e0ccc0731212f022
Jmz <jmztaylor@gmail.com>
2013-11-09 16:28:03 -0500

    first commit

---
+++ getdownload.php
@@ -0,0 +1,157 @@
+<?php
+/*
+ * Androtransfer.com Download Center
+ * Copyright (C) 2012   Daniel Bateman
+ * Copyright (C) 2013   James Taylor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require ('config.php');
+
+	// Get the filename given by directory linker
+	$fileget = $_GET["file"];
+
+	if (substr($fileget, 0, 1) == '/' || !isset($_GET['file']) || (substr($fileget, 0, 3) == '../')) {
+                header( 'Location: http://www.' . $siteName ) ;
+        } else {
+                $file = $fileget;
+        }
+
+	if(empty($_SERVER['REQUEST_URI'])) {
+    	$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'];
+	}
+
+	// Strip off query string so dirname() doesn't get confused
+	$url = preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']);
+	$folderpath = 'http://'.$_SERVER['HTTP_HOST'].'/'.ltrim(dirname($url), '/');
+//yay add some salt
+        $s1 = md5('GetAndroidFilesToday1343645291935HelloSaltyummy12!@$@');
+        $s2 = rand();
+        $s3 = md5($s2.rand().$s1);
+// Generate the unique download key
+	$key = $s1.$s2.uniqid(md5(rand())).$s3;
+
+// Get the activation time
+	$time = date('U');
+
+// Write the key and activation time to the database as a new row.
+	$registerid = mysql_query("INSERT INTO downloadkey (uniqueid,timestamp,filename) VALUES(\"$key\",\"$time\",\"$file\")") or die(mysql_error());
+?>
+
+<DOCUTYPE html>
+<head>
+<title><?php echo "Downloading " . basename($file); ?></title>
+</head>
+<body>
+<center>
+<div id='header'>
+        <a href="http://<?php echo $siteName ?>" ><img src="/images/header.jpg" width="900" height="182" /></a>
+    </div>
+<br>
+ <script type="text/javascript"><!--
+google_ad_client = "<?php echo $adsense ?>";
+/* Files */
+google_ad_slot = "3775827379";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<br><br><br>
+
+<?php
+$files2 = "files/" . $file;
+if (file_exists($files2)) {
+// Short URLs
+$url = $siteName . "/getdownload.php?file=" . $file;
+$format = 'simple';
+$api_url = 'http://androidfil.es/yourls-api.php';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_HEADER, 0);            // No header in the result
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return, do not echo result
+curl_setopt($ch, CURLOPT_POST, 1);              // This is a POST request
+curl_setopt($ch, CURLOPT_POSTFIELDS, array(     // Data to POST
+                'url'      => $url,
+                'format'   => $format,
+                'action'   => 'shorturl',
+                'username' => $username,
+                'password' => $password
+        ));
+
+// Fetch and return content
+$data = curl_exec($ch);
+curl_close($ch);
+$pageURL = "http://www.androidfiles.org/getdownload.php?file=" . $file;
+$filename = basename($file);
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
+echo "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-text=" . $filename . " data-url=" . $data . " data-via=\"$twitusername\" data-size=\"large\">Tweet</a>\n";
+echo "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";f$\n";
+echo "</script>";
+?>
+<a href="https://twitter.com/JmzAF" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @<?php echo $twitusername ?></a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";
+fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<?php
+echo "<br><h1>";
+echo "Downloading";
+echo "<br><br>";
+echo '<a href=' . $data . '>' . $filename . '</a>';
+echo "<br><br>";
+$query = sprintf("SELECT * FROM md5sums WHERE filename= '%s'",
+mysql_real_escape_string($file));
+$result = mysql_query($query) or die(mysql_error());
+$row = mysql_fetch_array($result);
+$something = "files/" . $file;
+        if (!$row['md5']) {
+                $md5 = md5_file("$something");
+                $sqlread = mysql_query("INSERT INTO md5sums (filename,md5) VALUES(\"$file\",\"$md5\")") or die(mysql_error());
+                echo "MD5: " . $md5;
+        }else{
+                echo "MD5: " . $row['md5'];
+}
+echo "<br><br>";
+echo "Downloads: " . $row['downloads'];
+mysql_close();
+?>
+<br><br>
+<?php
+// Redirect to the download
+echo '<META HTTP-EQUIV="Refresh" Content="2; URL=download.php?id=' . $key . '">';
+//show HTML below for 5 seconds
+} else {
+echo "<h1>File doesn't exist";
+echo "<br><a href=http://www.androidfiles.org>Return to homepage</a></h1>";
+}
+?>
+<br><br>
+<script type="text/javascript"><!--
+google_ad_client = "<?php echo $adsense ?>";
+/* Files */
+google_ad_slot = "3775827379";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<br><br>
+
+</center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,216 @@
+<?php
+/*
+ * Androtransfer.com Download Center
+ * Copyright (C) 2012   Daniel Bateman
+ * Copyright (C) 2013   James Taylor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'config.php';
+require_once 'markdown.php';
+
+$currentFolder = NULL;
+$currentDeveloper = NULL;
+
+if(isset($_GET['developer'])) {
+$currentDeveloper = $_GET['developer'];
+if(!in_array($currentDeveloper, $users))
+    die("Access denied.");
+} else {
+$currentDeveloper = false;
+}
+if(isset($_GET['folder'])) {
+$currentFolder = $_GET['folder'];
+if(strpos($currentFolder, '..') !== false)
+    die("Access denied.");
+$totalPath = null;
+}
+
+$fileMTimes = array();
+
+define("FILE_FILTER_FILES", 0x1);
+define("FILE_FILTER_DIRS", 0x2);
+define("FILE_FILTER_ALL", FILE_FILTER_DIRS | FILE_FILTER_FILES);
+function getAllInFolder($folder, $filter=FILE_FILTER_ALL) {
+    global $globalBlacklist;
+    $handle = opendir($folder);
+    $entries = array();
+    if ($handle) {
+        while (false !== ($entry = readdir($handle))) {
+            $entryPath = $folder."/".$entry;
+            if ($entry[0] == '.')
+                continue;
+            if (in_array($entry, $globalBlacklist))
+                continue;
+
+            if ((is_dir($entryPath) && $filter & FILE_FILTER_DIRS) ||
+                (!is_dir($entryPath) && $filter & FILE_FILTER_FILES)) {
+                $entries[] = $entry;
+            }
+        }
+        closedir($handle);
+    }
+    return $entries;
+}
+
+function sizePretty($bytes) {
+    if($bytes >= GB)
+        return number_format($bytes/GB) . " GB";
+    else if($bytes >= MB)
+        return number_format($bytes/MB) . " MB";
+    else if($bytes >= KB)
+        return number_format($bytes/KB) . " KB";
+    return number_format($bytes) . " bytes";
+}
+
+if ($currentDeveloper) {
+    $devPath = $baseDir."/".$currentDeveloper;
+    $subFolders = getAllInFolder($devPath, FILE_FILTER_DIRS);
+    sort($subFolders);
+
+    if (!$currentFolder) {
+        $currentFolder = '.';
+    }
+
+    if ($currentFolder) {
+        $folderPath = $devPath."/".$currentFolder;
+        $totalPath = $folderPath;
+        $files = getAllInFolder($folderPath, FILE_FILTER_FILES);
+        $handle = opendir($folderPath);
+
+    }
+}
+if(!empty($files)) {
+sort($files);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $siteName ?></title>
+    <link type='text/css' rel='stylesheet' href='style.css'/>
+</head>
+<body>
+<center>
+<script type="text/javascript"><!--
+google_ad_client = "<?php echo $adsense ?>";
+/* Top Bar */
+google_ad_slot = "3775827379";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+    <div id='header'>
+        <a href="http://<?php echo $siteName ?>" ><img src="/images/header.jpg" width="900" height="182" /></a>
+    </div>
+    <br>
+    <div id='links' class='block'>
+        <h2>Select a device</h2>
+        <?php foreach($users as $user): ?>
+        <a href='?developer=<?= $user ?>'><?= $user ?></a>
+        <?php endforeach ?>
+        <div style='clear: both'></div>
+    </div>
+
+    <div id='page'>
+        <?php if($currentDeveloper): ?>
+            <div id='sidebar'>
+                <div class='block'>
+                    <h2><?= htmlspecialchars($currentDeveloper) ?></h2>
+                    <ul>
+                    <?php foreach($subFolders as $folder): ?>
+                        <li class='<?= $currentFolder == $folder ? "active" : "" ?>'><a href='?developer=<?= rawurlencode($currentDeveloper) ?>&amp;folder=<?= rawurlencode($folder) ?>'><?= $folder ?></a><li>
+                    <?php endforeach ?>
+                    </ul>
+                </div>
+            </div>
+
+            <?php if($currentFolder): ?>
+                <div style='float: left; margin-left: 10px; width: 668px'>
+                    <div class='block'>
+                        <h2><?= htmlspecialchars($currentFolder) ?></h2>
+                        <?php if (count($files) > 0): ?>
+                            <table>
+                                    <tr>
+					<th align='left'>File</th>
+                                        <th align='left' width='120px' style='padding-right: 50px'>Last Mod.</th>
+                                        <th align='left' width='80px'>Size</th>
+                                        <th align='right' width='80px'>Downloads</th>
+                                    </tr>
+                                    <?php foreach($files as $file): ?>
+                                        <?php
+                                        $rp = realpath($totalPath . "/" . $file);
+                                        $resolvedPath = substr($rp, strpos($rp, "/files")+strlen("/files/"));
+					$filePath = $baseDir . "/" . $resolvedPath;
+					$query = sprintf("SELECT * FROM md5sums WHERE filename= '$resolvedPath'",
+                                        mysql_real_escape_string($file));
+                                        $result = mysql_query($query) or die(mysql_error());
+                                        $row = mysql_fetch_array($result);
+                                        $filedown = $row['downloads'];
+                                        ?>
+                                        <tr class='download'>
+                                            <td>
+                                                <div class='name'><a style='display: block' href='getdownload.php?file=<?= $resolvedPath ?>'><?= $file ?><br></a></div>
+                                            </td>
+						<td><?= date("F dS Y", filemtime("files/" . $resolvedPath)) ?></td>
+                                                <td><?= sizePretty(filesize($filePath)) ?></td>
+						<td><?= $filedown ?></td>
+                                        </tr>
+                                    <?php endforeach ?>
+                            </table>
+                        <?php else: ?>
+                            No files here.
+                        <?php endif ?>
+                    </div>
+
+            <?php endif ?>
+        <?php else: ?>
+            <div id='content'>
+                Click a link at the top to view each device's files.
+            </div>
+        <?php endif ?>
+        <div style='clear: both'></div>
+    </div>
+</a></div>
+<center>
+<br>
+<?php
+$result = mysql_query('SELECT SUM(downloads) AS value_sum FROM md5sums');
+$row = mysql_fetch_assoc($result);
+$sum = $row['value_sum'];
+echo "Total Downloads: " . $sum;
+mysql_close();
+?>
+<br>
+<br>
+<script type="text/javascript"><!--
+google_ad_client = "<?php echo $adsense ?>";
+/* Top Bar */
+google_ad_slot = "3775827379";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<br>
+<center>
+</body>
+</html>
